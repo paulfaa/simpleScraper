9fa493d50a3b83c8fd10b533a1708361e9097d52
paulfaa <paulfarrell12@gmail.com>
2020-08-08 21:24:25 +0100

    Added try/except for I/O

--- scraper.py
+++ scraper.py
@@ -1,39 +1,62 @@
+#python 3.8.4
+#need to run in virtual environment
+#scrape tool using AWS lamda and SNS
+
 from bs4 import BeautifulSoup
 import requests
 import json
+from datetime import datetime
 
-url = 'https://trustplanning.world/used-car-inventory-list/'
-response = requests.get(url, timeout = 5)
-content = BeautifulSoup(response.content, "html.parser")
+def getData():
+	url = 'https://trustplanning.world/used-car-inventory-list/'
+	try:
+		response = requests.get(url, timeout = 5)
+		content = BeautifulSoup(response.content, "html.parser")
+	except:
+		print("Connection failed - check URL")
+	carArray = []	
+	cars = content.findAll('div', attrs={"class": "su-post"})
+	for car in cars:
 
-# cars = content.findAll('div', attrs={"class": "su-post"})
-# for car in cars:
-	# print(car)
-	
-# carArray = []	
-# for car in content.findAll('div', attrs={"class": "su-post"})
-	# carObect = {
-		# "model": car.find('a', attrs={"class": "su-post-title").text.encode('utf-8'),
-		# "dateAdded": car.find('div', attrs={"class": "su-post-meta").text.encode('utf-8')
-	# }
-# print(carObect)
+		#carObject = {
+		#		"model": car.find('a', attrs={"class": "su-post-title"}),
+		#		"dateAdded": car.find('div', attrs={"class": "su-post-meta"})
+		#	}
+		dateAdded = car.find('div', attrs={"class": "su-post-meta"}).text
+		titles = car.find('h2', attrs={"class": "su-post-title"})
+		titleText= titles.find('a').contents[0]
+		carObject = {
+		"model": titleText,
+		"dateAdded": dateAdded.strip('\n\t').replace('Posted: ','')}
+		carArray.append(carObject)
+	print(carArray)
+	try:
+		with open('carList.json', 'w') as outfile:
+			json.dump(carArray, outfile)
+	except:
+		print("Write to file failed")
+		
+def parseData():
+	t = datetime.now()
+	todayString = t.strftime("%Y-%m-%d")
+	carAdded = False
+	try:
+		with open ('carList.json') as j:
+			jData = json.load(j)
+			
+			for x in jData:
+				if "bnr32" in x['model'].lower():
+					print(x)
+				if x['dateAdded'] == todayString:
+					carAdded = True
+	except:
+		print("Open file failed")
 
-carArray = []	
-cars = content.findAll('div', attrs={"class": "su-post"})
-for car in cars:
-
-	#carObject = {
-	#		"model": car.find('a', attrs={"class": "su-post-title"}),
-	#		"dateAdded": car.find('div', attrs={"class": "su-post-meta"})
-	#	}
-	dateAdded = car.find('div', attrs={"class": "su-post-meta"}).text
-	titles = car.find('h2', attrs={"class": "su-post-title"})
-	titleText= titles.find('a').contents[0]
-	carObject = {
-	"model": titleText,
-	"dateAdded": dateAdded.strip('\n\t').replace('Posted: ','')}
-	carArray.append(carObject)
-print(carArray)
-
-with open('carList.json', 'w') as outfile:
-	json.dump(carArray, outfile)
+	if carAdded:
+		print("New bnr32 added today")
+		#call aws ses
+	else:
+		print("No new bnr32 added today")
+				
+getData()
+parseData()
